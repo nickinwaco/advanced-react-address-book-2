2d2cab9f1f28b8469201d5380e58ae85b81c0c0b
Nick Rodriguez <Nick Rodriguez>
2017-05-07 17:46:06 -0500

    GetfilteredList 05072017

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 
 function SearchBar(props) {
@@ -6,10 +7,15 @@ function SearchBar(props) {
   return (
     <form>
       <input type="text" name="query" onChange={(event) => {
-        console.log(event);
+        console.log(event.target.value);
+        props.onChange(event.target.value);
       }} />
     </form>
   );
 }
+
+SearchBar.propTypes = {
+  onChange: PropTypes.func.isRequired
+};
 
 export default SearchBar;
